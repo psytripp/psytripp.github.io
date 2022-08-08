9ecef0f304d45464a8e918c86afb677d50f6166a
psytripp <miguel.zunigam@usach.cl>
2022-08-07 23:49:14 -0400

    ACTUALIZACION JQUERY

---
+++ main.js
@@ -0,0 +1,33 @@
+$(function(){
+
+$("#btn-alert").click(function(){
+        alert("Estamos recolectando la mayor cantidad de información, tanto para el diseño como contenido de la página. Todo esto lo hacemos por ustedes. Con cariño, MZM 2022.");
+    
+
+
+});
+
+$("#btn-hide").click(function(){
+
+    $("#p1").hide();
+
+
+});
+
+$("#btn-show").click(function(){
+
+    $("#p1").show();
+
+
+});
+
+$("#btn-remove").click(function(){
+
+    $("#rem").empty();
+
+
+});
+
+
+
+});
