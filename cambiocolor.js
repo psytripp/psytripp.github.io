4f344ad0c61b8fec4842e660879d5bdf01312548
psytripp <miguel.zunigam@usach.cl>
2022-08-07 22:13:58 -0400

    js 2

---
+++ cambiocolor.js
@@ -0,0 +1,9 @@
+let color = document.getElementById("menu");
+menu.addEventListener("mouseover", function(event) {
+    event.target .style.background = "#FA8072";
+})
+
+let color2 = document.getElementById("menu");
+menu.addEventListener("mouseout", function(event) {
+    event.target .style.background = "#0275d8";
+})
