8fea27e3a05a85b1acc2fd9649ec8a428434bee1
psytripp <miguel.zunigam@usach.cl>
2022-09-26 00:40:59 -0300

    actualizacion

---
+++ .java
@@ -0,0 +1,19 @@
+/* public static void main(String[] args) {
+    
+
+String cadena="Estas es una Cadena entera"
+
+char c = cadena.charAt(6);
+
+for (int i=0<cadena.lenght());i++){
+
+c=cadena.charAt.(i);
+System.out.printIn(""+c)
+
+
+
+}
+
+}
+
+*/
